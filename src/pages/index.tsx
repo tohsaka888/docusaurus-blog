9a5fd1d9f4aeb0a60a3bf6c69cb2c46fd41039c2
tohsaka888 <156132264@qq.com>
2022-02-08 14:33:33 +0800

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,7 +34,6 @@ export default function Home(): JSX.Element {
       <Background>
         <HomepageHeader />
       </Background>
-
       {/* <main>
         <HomepageFeatures />
       </main> */}
